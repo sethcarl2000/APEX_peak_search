cfa53270201aa2f649b960419eb0a13f6351b15e
sethcarl2000 <sethhall@jlab.org>
2026-06-08 13:58:30 -0400

    Change interface of Exponential poly to make the user specify the sized of a raw array

--- ExponentialPoly.hpp
+++ ExponentialPoly.hpp
@@ -14,7 +14,7 @@ namespace peak_search
 struct ExponentialPoly {
     //coefficients are stored in the following order: 
     // f(x) = std::exp( coeffs[0] + coeffs[1]*x + coeffs[2]*x*x + coeffs[3]*x*x*x + ... )
-    std::vector<double> coeffs; 
+    std::vector<double> coeffs{}; 
 
     inline double operator()(double x) const {
         if (coeffs.empty()) return numbers::nan; 
@@ -26,16 +26,16 @@ struct ExponentialPoly {
 
     //coefficients are stored in the following order: 
     // f(x) = std::exp( coeffs[0] + coeffs[1]*x + coeffs[2]*x*x + coeffs[3]*x*x*x + ... )
-    inline static double Eval(double x, double *coeffs) {
-        int n; 
-        //check if the array of coefficients is null, or if its size is less than one (if so, return nan)
-        if (!coeffs || (n = ((int)(sizeof(coeffs)/sizeof(double)))) < 1) return numbers::nan; 
-
+    inline static double Eval(double x, double *coeffs, int n) {
+        if (!coeffs) return numbers::nan;
+        
         double poly = coeffs[n-1];
         for (int i=n-2; i>=0; i--) { poly = coeffs[i] + poly*x; }
         return std::exp( poly );
     }
 };
+
+static_assert(std::is_default_constructible<ExponentialPoly>::value, "ExponentialPoly struct is not default constructible"); 
 
 }; 
 
