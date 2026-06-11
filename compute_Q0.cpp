ec608e7235fc35995d36e3d79d7d79568688de83
sethcarl2000 <sethhall@jlab.org>
2026-06-10 23:15:21 -0400

    Add 'compute_Q0' fcn

---
+++ compute_Q0.cpp
@@ -0,0 +1,61 @@
+
+#include "compute_Q0.hpp"
+#include "numbers.hpp"
+#include "fit_parameter.hpp"
+#include "best_likelihood_fit.hpp"
+#include "bininfo.hpp"
+
+#include <TError.h>
+#include <TString.h> 
+#include <Math/ProbFuncMathCore.h>
+
+#include <cmath> 
+
+
+namespace peak_search
+{
+
+double compute_Q0(TH1D* hist, const std::function<double(double,double,const double*)>& fcn, std::vector<double>& nuissance_params)
+{
+    //extract the histogram values
+    if (!hist) {
+        Error(__func__, "hist passed is null");
+        return numbers::nan;
+    }
+
+    //first, compute the best fit
+    std::vector<fit_parameter_t> fit_params; fit_params.reserve(nuissance_params.size() + 1);
+
+    fit_params.push_back({ .val=0., .name="mu" }); 
+
+    auto& param_mu = fit_params[0]; 
+
+    for (size_t i=0; i<nuissance_params.size(); i++) {
+        fit_params.push_back({ .val=nuissance_params[i], .name=Form("param_%zi",i), .is_fixed=false});
+    }
+
+    //first, compute the NLL with mu=0
+    auto data = copy_1D_hist(hist);
+
+    auto wrapper_fcn = (std::function<double(double,const double*)>)[&fcn](double x, const double *par){ return fcn(x, par[0], par+1); };
+    
+    //now, fix mu=0, and find the NLL
+    param_mu.val =0.; 
+    param_mu.is_fixed = true; 
+    double NLL_0      = best_likelihood_fit(data, wrapper_fcn, fit_params);
+    
+    param_mu.is_fixed = false; 
+    double NLL_best   = best_likelihood_fit(data, wrapper_fcn, fit_params); 
+
+    //return the computed value of Q0 
+    double Q0 = NLL_0 - NLL_best; 
+    if (param_mu.val < 0) { Q0 *= -2.; } else { Q0 *= +2.; }
+    return Q0; 
+}
+double compute_Q0_p(double Q0)
+{
+    double p = ROOT::Math::normal_cdf_c( std::sqrt(std::fabs(Q0)), 1. );
+    return (Q0 > 0) ? p : 1 - p; 
+}
+
+};
---
+++ compute_Q0.hpp
@@ -0,0 +1,28 @@
+#ifndef peak_search_compute_Q0_hpp
+#define peak_search_compute_Q0_hpp
+
+#include <pdf_fcn.hpp>
+
+#include <TH1D.h> 
+#include <TH2D.h> 
+
+#include <vector> 
+
+namespace peak_search 
+{
+
+/// @brief Compute the 'Q0' statistic 
+/// @param hist histogram representing data 
+/// @param pdf PDF of expecation value for each bin 
+/// @param nuissance_params first-guess of nuissance parameters to fit. 
+/// @return Q0 statistic (representing degree of agreement with no-signal hypothesis)
+double compute_Q0(TH1D* hist, const std::function<double(double,double,const double*)>& fcn, std::vector<double>& nuissance_params); 
+
+/// @brief returns the p-value for a computed Q0
+/// @param Q0 a computed value of 'Q0' 
+/// @return p-value (prob. that any observation of Q0 is as great or greater than passed value)
+double compute_Q0_p(double Q0); 
+
+};
+
+#endif
