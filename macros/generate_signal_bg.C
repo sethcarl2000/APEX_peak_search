bb00ea1f7beed3349f627d68fe4240c1858d74e8
Seth Hall <sethhall@jlab.org>
2026-07-22 16:27:55 -0400

    Add version of 'generate_signal_bg' that fits a small gaussian peak

--- macros/generate_signal_bg.C
+++ macros/generate_signal_bg.C
@@ -3,6 +3,10 @@
 
 #include <bininfo.hpp>
 #include <fit_exponential_poly.hpp>
+#include <Fcn1D/Gauss.hpp>
+#include <Fcn1D/FcnSum.hpp>
+#include <newton_optimizer.hpp> 
+#include <fit_parameter.hpp> 
 //ROOT headers
 #include <TRandom3.h> 
 #include <TH1D.h> 
@@ -66,7 +70,7 @@ void generate_signal_bg(unsigned int n_bg, unsigned int n_signal)
 
 
     new TCanvas; 
-    hist->Draw(); 
+    hist->Draw("E"); 
 
     hist->GetYaxis()->SetRangeUser(0., hist->GetMaximum()*1.1); 
 
@@ -74,22 +78,55 @@ void generate_signal_bg(unsigned int n_bg, unsigned int n_signal)
 
     auto exp_poly = (peak_search::ExponentialPoly)peak_search::fit_exponential_poly(hist, 6); 
 
+    const double dx = (xmax - xmin)/((double)hist->GetXaxis()->GetNbins()); 
+
+    auto fcn_poly = (std::function<double(double*,double*)>)[exp_poly,dx](double *x, double *par){ 
+        //std::cout << "<tf1>: dx = " << dx << "\n"; 
+        return dx * exp_poly(x[0]);
+    }; 
+
+    auto tf1 = new TF1("exp_poly", fcn_poly, xmin, xmax, 0); 
+
     int i=0; 
     std::cout << "coeffs:\n"; 
     for (auto& coeff : exp_poly.GetParams()) {
         std::printf("   %3i : %f\n", i++, coeff); 
     }
 
-    const double dx = (xmax - xmin)/((double)hist->GetXaxis()->GetNbins()); 
+    auto gaussian_fcn = peak_search::Gauss(0, x0_signal, sigma_signal); 
 
-    auto fcn = (std::function<double(double*,double*)>)[exp_poly,dx](double *x, double *par){ 
-        //std::cout << "<tf1>: dx = " << dx << "\n"; 
-        return dx * exp_poly(x[0]);
-    }; 
 
-    auto tf1 = new TF1("exp_poly", fcn, xmin, xmax, 0); 
+    std::cout << "exp poly dof: " << exp_poly.GetDoF() << " size: " << exp_poly.GetParams().size() << "\n"; 
 
+    auto fcn_s_plus_b = peak_search::FcnSum(&gaussian_fcn, &exp_poly); 
+
+    tf1->SetLineStyle(kDashed); 
     tf1->Draw("SAME"); 
+
+
+    auto coeffs_vec = fcn_s_plus_b.GetParams(); 
+    std::vector<fit_parameter_t> coeffs; coeffs.reserve(coeffs_vec.size()); 
+    
+    coeffs.emplace_back(fit_parameter_t{ .val = coeffs_vec[0], .name = "mu", .is_fixed = false });
+    for (size_t i=1; i<coeffs_vec.size(); i++) {
+        coeffs.emplace_back(fit_parameter_t{ .val = coeffs_vec[i], .name = Form("c%zi",i), .is_fixed = false }); 
+    }
+    peak_search::newton_optimizer(data, fcn_s_plus_b, coeffs); 
+
+
+    
+    auto fcn = (std::function<double(double*,double*)>)[exp_poly, gaussian_fcn, dx](double *x, double *par){ 
+        //std::cout << "<tf1>: dx = " << dx << "\n"; 
+        return dx * ( exp_poly(x[0]) + gaussian_fcn(x[0]) );
+    }; 
+    auto tf1_s_b = new TF1("f_s_b", fcn, xmin, xmax, 0); 
+
+    tf1_s_b->SetLineColor(kBlue); 
+    tf1_s_b->SetLineStyle(kSolid); 
+
+    tf1_s_b->Draw("SAME"); 
+
+    return; 
 
 }
 
