5b03929fc897aef5753e36934949f4fc51ce0c6f
Seth Hall <sethhall@jlab.org>
2026-08-21 09:29:29 -0400

    Add subrage-selection for 'make_histogram_copy'

--- include/make_histogram_copy.hpp
+++ include/make_histogram_copy.hpp
@@ -1,9 +1,15 @@
 #include <Histo1D.hpp>
+#include <numbers.hpp>
 // ROOT headers
 #include <TH1D.h> 
 
 namespace peak_search {
 
-Histo1D make_histogram_copy(TH1D* hist_source);
+/// @brief Make a copy of a TH1D (as a Histo1D)
+/// @param hist_source source histogram to  get the data from
+/// @param xmin min x-value to include (if it cuts a bin in half, the whole bin is included). if NaN is passed, or outside source hist range, lower bound is source hist lower bound. 
+/// @param xmax max x-value to include (if it cuts a bin in half, the whole bin is included). if NaN is passed, or outside source hist range, upper bound is source hist upper bound. 
+/// @return 
+Histo1D make_histogram_copy(TH1D* hist_source, double xmin=numbers::nan, double xmax=numbers::nan);
 
 };
--- src/make_histogram_copy.cpp
+++ src/make_histogram_copy.cpp
@@ -1,15 +1,16 @@
 
 #include <make_histogram_copy.hpp>
-//stdlib headers
-#include <stdexcept>
 //ROOT headers
 #include <TError.h>
 #include <TAxis.h>
+//stdlib headers
+#include <stdexcept>]
+#include <cmath> 
 
 namespace peak_search
 {
 
-Histo1D make_histogram_copy(TH1D* hist)
+Histo1D make_histogram_copy(TH1D* hist, double xmin, double xmax)
 {
     //make a copy of the histogram passed to us 
     Histo1D copy; 
@@ -19,17 +20,34 @@ Histo1D make_histogram_copy(TH1D* hist)
         throw std::invalid_argument(Form("in <%s>: histogram passed is null!", __func__)); 
         return copy; 
     }
+
     
     auto xax = hist->GetXaxis();
 
-    const int nbins = xax->GetNbins(); 
+    int min_bin, max_bin; 
 
-    copy.bins.reserve(nbins);
+    //find minimum bin
+    if (numbers::is_nan(xmin)) {
+        min_bin = 1;  
+    } else {
+        xmin = std::max(xmin, xax->GetXmin());
+        min_bin = xax->FindBin(xmax);
+    }
+
+    //find maximum bin
+    if (numbers::is_nan(xmax)) {
+        max_bin = xax->GetNbins();  
+    } else {
+        xmax = std::min(xmax, xax->GetXmax());
+        max_bin = xax->FindBin(xmax);
+    }
+
+    copy.bins.reserve(max_bin - min_bin + 1);
 
     double x = xax->GetXmin(); 
-    double dx = (xax->GetXmax() - xax->GetXmin())/((double)nbins); 
+    double dx = (xax->GetXmax() - xax->GetXmin())/((double)xax->GetNbins()); 
 
-    for (int i=1; i<=nbins; i++) { 
+    for (int i=min_bin; i<=max_bin; i++) { 
         copy.bins.emplace_back( 
             x,          //xmin
             x += dx,    //xmax
